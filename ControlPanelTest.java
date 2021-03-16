4cc26b9c1da8009baf6eab5e1fa3ce007be55977
Atlantiks <atlantiks@bigmir.net>
2021-03-16 22:34:21 +0200

    Sout changed back to objects

--- ControlPanelTest.java
+++ ControlPanelTest.java
@@ -23,7 +23,8 @@ class ControlPanel {
     private AirGauge airGauge   = new AirGauge();
 
     public void control() {
-        System.out.println("Measure height Measure air pressure");
+        System.out.println(altimeter);
+        System.out.println(airGauge);
     }
 
 }
